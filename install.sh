e0ccf74ac8980e9d49ecafdd3712dab9edc8adcc
Chuck Greb <charles.greb@gmail.com>
2015-08-03 14:48:58 -0400

    Modify install script to support OS X

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ echo "Installing Mapzen code style configs..."
 
 for i in $HOME/.IntelliJIdea*/config/codestyles \
          $HOME/.IdeaIC*/config/codestyles \
-         $HOME/.AndroidStudio*/config/codestyles
+         $HOME/.AndroidStudio*/config/codestyles \
+         $HOME/Library/Preferences/IntelliJIdea*/codestyles \
+         $HOME/Library/Preferences/IdeaIC*/codestyles \
+         $HOME/Library/Preferences/AndroidStudio*/codestyles
+
 do
   cp -frv $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/configs/* $i 2> /dev/null
 done
